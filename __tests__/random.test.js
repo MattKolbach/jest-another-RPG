174b7e2099aa4b9a28aa2a7fe50417144fc65000
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-01 12:55:49 -0600

    end 10.1.4 first tests work

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,7 +1,19 @@
-const { TestWatcher } = require("jest");
-const checkIfEqual = require("../lib/random.js");
+//const { test } = require('jest'); I don't remember where this came from...
+/*
+const checkIfEqual = require('../lib/random.js');
 
-TestWatcher("checks if 10 is equal to 10", () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
+test('checks if 10 is equal to 10', () => {
+  expect(checkIfEqual(10, 10)).toBe(true);
 });
+*/
+/////  new test  /////
 
+const randomNumber = require('../lib/random.js');
+//const { test } = require('picomatch');
+//const { expect } = require('@jest/globals');
+//const { random } = require('lodash');
+
+test('gets random number between 1 and 10', () => {
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+});
