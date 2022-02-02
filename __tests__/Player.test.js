8b7767149fcc703db6264bca12c705adf3ce261e
Matt Kolbach <mattkolbach@yahoo.com>
2022-02-02 12:30:33 -0600

    added mock potion file, added inventory to Player, End 10.2.5

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,8 @@
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
 const Player = require('../lib/Player.js');
 
 test('creates a player object', () => {
@@ -7,5 +12,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
 
